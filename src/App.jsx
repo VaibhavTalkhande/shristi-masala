20e970fa89cd49398505225441cd9aceb368675c
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2025-07-03 01:27:23 +0530

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   if (loading) {
     return (
       <div className="fixed inset-0 flex flex-col items-center justify-center bg-white z-[9999]">
-        <img src={logo} alt="Shrishti Gold Masale Logo" className=" mb-6  bg-white p-2 " />
+        <img src={logo} alt="Shrishti Gold Masale Logo" className=" mb-6 h-60 w-60 bg-white p-2 " />
         <span className="loading loading-spinner loading-xl text-[#a10000] mb-6"></span>
         <h1 className="text-2xl md:text-3xl font-bold text-[#a10000] font-serif text-center">Welcome to <span className="text-[#e0d53c]">Shrishti Gold Masale</span></h1>
       </div>
